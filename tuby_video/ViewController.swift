6b065542320d5a20f35d9e68cb8888c6492f1e18
Abdelrahman Aswa <abdoaswa4@icloud.com>
2021-05-16 09:21:25 +0200

    initial xcode project creation

---
+++ tuby_video/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tuby_video
+//
+//  Created by abdelrahman aswa ahmed  on 16/05/2021.
+//  Copyright © 2021 abdelrahman aswa ahmed . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
